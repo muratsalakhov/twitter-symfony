4342c6d9d61c9551a5a268e4ad92eb5991cfc514
muratsalakhov <muratsalakhov@gmail.com>
2024-04-01 22:53:40 +0300

    refact: BaseController -> AbstractController

--- src/Shared/Presentation/Controller/AbstractController.php
+++ src/Shared/Presentation/Controller/AbstractController.php
@@ -6,9 +6,9 @@ namespace App\Shared\Presentation\Controller;
 
 use App\Shared\Presentation\Trait\ResponseTrait;
 use App\Shared\Presentation\Trait\ValidationTrait;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as BaseController;
 
-abstract class BaseController extends AbstractController
+abstract class AbstractController extends BaseController
 {
     use ValidationTrait;
     use ResponseTrait;
