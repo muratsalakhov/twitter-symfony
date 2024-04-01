5f5612f4de5d5bd7e42a7e53b8812831626cdd65
muratsalakhov <muratsalakhov@gmail.com>
2024-04-01 22:11:33 +0300

    feat: Трейт для валидации данных в контроллере

---
+++ src/Shared/Presentation/Exception/ValidationException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Presentation\Exception;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class ValidationException extends Exception
+{
+    public function __construct(string $message = "Validation exception", int $code = Response::HTTP_UNPROCESSABLE_ENTITY, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
