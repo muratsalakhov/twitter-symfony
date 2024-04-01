8aaa19a2dfbd7bd745610b53a9154bb53f8dc47c
muratsalakhov <muratsalakhov@gmail.com>
2024-04-01 22:21:59 +0300

    feat: Трейт для формата ответа

--- src/Shared/Presentation/Controller/BaseController.php
+++ src/Shared/Presentation/Controller/BaseController.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Shared\Presentation\Controller;
 
+use App\Shared\Presentation\Trait\ResponseTrait;
 use App\Shared\Presentation\Trait\ValidationTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 abstract class BaseController extends AbstractController
 {
     use ValidationTrait;
+    use ResponseTrait;
 }
