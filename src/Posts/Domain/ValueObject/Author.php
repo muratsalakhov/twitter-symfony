216bfaae945f822dc3f0fcc2f6d98b67632b8b1d
muratsalakhov <muratsalakhov@gmail.com>
2024-04-03 23:32:12 +0300

    feat: Сущность автора

---
+++ src/Posts/Domain/ValueObject/Author.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Posts\Domain\ValueObject;
+
+class Author
+{
+    private string $id;
+
+    private string $name;
+
+    public function __construct(string $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
