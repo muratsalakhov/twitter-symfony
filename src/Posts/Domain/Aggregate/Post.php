4635e5d0ca40e3d50094fd1abce2a838333f5406
muratsalakhov <muratsalakhov@gmail.com>
2024-04-03 23:32:17 +0300

    feat: Сущность Поста

---
+++ src/Posts/Domain/Aggregate/Post.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Posts\Domain\Aggregate;
+
+use App\Posts\Domain\ValueObject\Author;
+use App\Shared\Domain\Service\UlidService;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'posts_posts')]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    private string $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $text;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: 'ulid')]
+    private string $authorId;
+
+    private Author $author; // todo
+
+    // private array $likes; todo
+
+    // private array $comments; todo
+
+    public function __construct(string $text, string $authorId, Author $author)
+    {
+        $this->id = UlidService::generate();
+        $this->text = $text;
+        $this->authorId = $authorId;
+        $this->author = $author;
+        $this->createdAt = new DateTimeImmutable();
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function getAuthorId(): string
+    {
+        return $this->authorId;
+    }
+
+    public function getAuthor(): Author
+    {
+        return $this->author;
+    }
+}
